3abc92a99d19c9fea506b7ba2dd7efb5d2637ee9
Kobeng Chantel <kobengchantel@gmail.com>
2025-01-17 23:28:20 +0200

    done

---
+++ OCPJava/printDayOfWeek.java
@@ -0,0 +1,35 @@
+//using switch statement
+
+public class printDayOfWeek {
+
+    public void printDayOfWeek(int day) {
+        switch (day) {
+            case 0:
+                System.out.print("Sunday");
+                break;
+            case 1:
+                System.out.print("Monday");
+                break;
+            case 2:
+                System.out.print("Tuesday");
+                break;
+
+
+            case 3:
+                System.out.print("Wednesday");
+                break;
+            case 4:
+                System.out.print("Thursday");
+                break;
+            case 5:
+                System.out.print("Friday");
+                break;
+            case 6:
+                System.out.print("Saturday");
+                break;
+            default:
+                System.out.print("Invalid value");
+                break;
+        }
+    }
+}
