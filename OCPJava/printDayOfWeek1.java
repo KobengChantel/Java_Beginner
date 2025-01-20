d19e08678a96249bdfd592499cae088bf272d18f
Kobeng Chantel <kobengchantel@gmail.com>
2025-01-20 23:41:04 +0200

    done

--- OCPJava/printDayOfWeek1.java
+++ OCPJava/printDayOfWeek1.java
@@ -1,26 +1,21 @@
-
-//using switc expression
-
-public class printDayOfWeek1 {
+public class PrintDayOfWeek1 {
     public void printDayOfWeek(int day) {
+        // Correctly format the switch expression
         var result = switch (day) {
-            case 0 - > "Sunday"
-                ;
-            case 1 - > "Monday"
-                ;
-            case 2 - > "Tuesday"
-                ;
-            case 3 - > "Wednesday"
-                ;
-            case 4 - > "Thursday"
-                ;
-            case 5 - > "Friday"
-                ;
-            case 6 - > "Saturday"
-                ;
-            default
-                - > "Invalid value";
+            case 0 -> "Sunday";
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid value";
         };
         System.out.print(result);
+    }
+
+    public static void main(String[] args) {
+        PrintDayOfWeek1 dayPrinter = new PrintDayOfWeek1();
+        dayPrinter.printDayOfWeek(3); // Example usage
     }
 }
