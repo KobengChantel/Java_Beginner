ce41e345c916057c15a081037c5d898295bb9859
Kobeng Chantel <kobengchantel@gmail.com>
2025-01-31 23:02:31 +0200

    done

---
+++ OCPJava/HasTail.java
@@ -0,0 +1,4 @@
+
+public interface HasTail {
+    public abstract boolean isTailStriped();
+}
---
+++ OCPJava/Marsupial.java
@@ -0,0 +1,5 @@
+class Marsupial {
+    protected int age = 2;
+    public static boolean isBiped() {
+        return false;
+    } }
