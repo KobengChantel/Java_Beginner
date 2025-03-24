6ef37e04932250b1d3cf28daa16491b666121ab4
Kobeng Chantel <kobengchantel@gmail.com>
2025-03-24 13:19:46 +0200

    Lesson 2

--- java_sg1/src/Chapter2/ex2_1_exercise/SayHello.java
+++ java_sg1/src/Chapter2/ex2_1_exercise/SayHello.java
@@ -2,6 +2,13 @@ package Chapter2.ex2_1_exercise;
 
 public class SayHello {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
     }
 }
+
+// sout to print anything
+//psvm or main for the main method
+//'' represents character
+//"" represents a string
