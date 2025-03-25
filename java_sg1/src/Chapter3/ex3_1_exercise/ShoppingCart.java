731ccb2a1f58193cc95443ecc63b3110f78c3b37
Kobeng Chantel <kobengchantel@gmail.com>
2025-03-25 11:45:29 +0200

    Lesson 3 done

--- java_sg1/src/Chapter3/ex3_1_exercise/ShoppingCart.java
+++ java_sg1/src/Chapter3/ex3_1_exercise/ShoppingCart.java
@@ -4,8 +4,9 @@ package Chapter3.ex3_1_exercise;
 
 
 public class ShoppingCart {
-    public static void main (String[] args){
-        System.out.println("Welcome to the Shopping Cart!");
+    public static void main (String[] args)
+    {
+        System.out.println("Welcome to the Shopping Cart!Enjoy your checkout!!");
     }
 }
 
