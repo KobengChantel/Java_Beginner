d5aba06b10d2fdec5e33fe18674728e60df004d4
Kobeng Chantel <kobengchantel@gmail.com>
2025-04-24 08:11:40 +0200

    Lesson done

--- Java_sg2/src/chapter14/Lesson14/TestClass.java
+++ Java_sg2/src/chapter14/Lesson14/TestClass.java
@@ -1,4 +1,33 @@
 package chapter14.Lesson14;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Jada
+ */
 public class TestClass {
+    public static void main(String[] args) {
+        TestClass tc = new TestClass();
+        try{
+            tc.testCheckedException();
+        }catch(IOException e) {
+            System.out.println(e);
+        }  catch (Exception ex) {
+            Logger.getLogger(TestClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void testCheckedException() throws Exception {
+        File testFile = new File("//testFile.txt");
+        testFile.createNewFile();
+        System.out.println("testFile exists: " +
+                testFile.exists());
+    }
 }
