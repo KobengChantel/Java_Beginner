b945aaaae4f15ba26e6540718d41ac863edcf6fa
Kobeng Chantel <kobengchantel@gmail.com>
2025-11-17 20:51:00 +0200

    done

--- OCPJava/java8/Java8Practice/src/test/java/practiceLesson2/PersonTest.java
+++ OCPJava/java8/Java8Practice/src/test/java/practiceLesson2/PersonTest.java
@@ -32,12 +32,38 @@ public class PersonTest {
     @Test
     public void shouldReturnNumberOfPersonsInLoop(){
         Person person1;
+        int i = 0;
+
+//        do {//condition is checked after execution
+//            i++;
+//        } while( i < 4);
+//        }
+
+        while( i < 4){
+            person1 = new Person();
+
+            i++;
+       }
         for( int i = 0; i < 4; i++ ){
             person1 = new Person();
-    }
+   }
        assertEquals(4, Person.numberOfPersons());
 
     }
+
+    //lesson 9
+    @Test
+    public void deonstrateArrays(){
+        Person [] persons = new Person[4];  //creating an array of type person that can hold umber of person
+    Person myPerson = new Person();
+        persons[0] = new Person();
+        persons[1] = new Person();
+        persons[2] = new Person();
+        persons[3] = new Person();
+        persons[3] = myPerson;
+        myPerson = persons[2];
+    }
+
 //lesson7
     @Test
     public void demonstrateBooleanLesson() {
