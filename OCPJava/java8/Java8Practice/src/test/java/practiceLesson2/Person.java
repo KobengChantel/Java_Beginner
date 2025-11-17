ea1943d8cbe523567ec7a1b4aa22e61be8dd5bb0
Kobeng Chantel <kobengchantel@gmail.com>
2025-11-17 20:08:11 +0200

    done

--- OCPJava/java8/Java8Practice/src/test/java/practiceLesson2/Person.java
+++ OCPJava/java8/Java8Practice/src/test/java/practiceLesson2/Person.java
@@ -4,23 +4,31 @@ import java.lesson3.Name;
 
 public class Person {
 
-        private Name personName;
+        private Name personName; //instance variable
+    private static int personCounter;
 
-        public Person(practiceLesson2.Name personName){
-            this.personName = personName;
-        }
+    public Person(Name personName){
+this.personName =  personName;
+    }
+//default constructor has no parameter
+// used to initialze an object
 
-        public Person (){
-            /*
-            empty on purpose default constructor
-             */
-            //single line comment
-        }
-        public String helloWorld(){
+    public Person(){
+        personCounter ++;
+        /* multi line comment
+        empty on purpose - default constructor
+         */
+        // single line comment
+    }
+        public String helloWorld(){ //instance method
         return "Hello World";
     }
 
-    public String hello(String name) {
-    return "Hello  "+ name;
-        }
+    public String hello(String marcus) {
+        return "Hello" + name; //+ to concatinate
+    }
+
+    public static int numberOfPersons(){
+        return personCounter;
+    }
 }
--- OCPJava/java8/Java8Practice/src/test/java/practiceLesson2/PersonTest.java
+++ OCPJava/java8/Java8Practice/src/test/java/practiceLesson2/PersonTest.java
@@ -3,17 +3,70 @@ package practiceLesson2;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class PersonTest  {
+public class PersonTest {
 
-  @Test
-  public void shouldReturnHelloWorld(){
-      Person marcus = new Person();
-      assertEquals("Hello World",marcus.helloWorld());
-  }
-@Test
-    public void shouldReturnMarcus(){
-      Person person = new Person();
-assertEquals("Hello  World",person.hello("Marcus"));
-}
+    @Test
+    public void shouldReturnHelloWorld() {
+        Person marcus = new Person();
+        assertEquals("Hello World", marcus.helloWorld());
+    }
 
+    @Test
+    public void shouldReturnMarcus() {
+        Person person = new Person();
+        assertEquals("Hello World", person.hello("Marcus"));
+
+    }
+//lesson6
+    @Test
+    public void shouldReturnNumberOfPersons() {
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+        Person person4 = new Person();
+        assertEquals(4, Person.numberOfPersons());
+
+    }
+
+    //lesson 8
+    @Test
+    public void shouldReturnNumberOfPersonsInLoop(){
+        Person person1;
+        for( int i = 0; i < 4; i++ ){
+            person1 = new Person();
+    }
+       assertEquals(4, Person.numberOfPersons());
+
+    }
+//lesson7
+    @Test
+    public void demonstrateBooleanLesson() {
+        int i = 4;
+        int j = 3;
+        boolean monday = false;
+        boolean raining = true;
+        if (shouldDrinkBeer(i, j, monday, raining)) {
+drinkBeer();
+        }
+        if(i == j){
+            raining = true;
+        }
+        if(monday || raining ){
+            i = 34;
+            j = 91;
+            i = i + j;
+            drinkbeer();
+        } else if(j > i){
+            i++;
+            j =56;
+        }else if ( i != j){
+            i = 8;
+        }
+else {
+    j--;
+        }
+    }
+    private boolean shouldDrinkBeer(int i, int j, boolean monday, boolean raining){
+        retun  i < j || ((monday && !raining) || j > 3);
+    }
 }
