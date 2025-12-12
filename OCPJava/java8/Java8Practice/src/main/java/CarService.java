79a88581577ba0d8200dc509aa8bdf3d7c662776
Kobeng Chantel <kobengchantel@gmail.com>
2025-12-12 22:13:57 +0200

    done

--- OCPJava/java8/Java8Practice/src/main/java/CarService.java
+++ OCPJava/java8/Java8Practice/src/main/java/CarService.java
@@ -1,5 +1,15 @@
-public class CarService {
-    public void process(String input){
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class CarService {
+
+    private final Logger LOG = LoggerFactory.getLogger(carService.class)
+
+    public void process(String input){
+        log.isDebugEnabled()) {
+            log.debug("processing car: " + input);
+            log.error("processing car: " + input);
+            log.warn("processing car: " + input);
+        }
     }
 }
