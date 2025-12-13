c017647ee18c951cb5cbfee111e76fc555000182
Kobeng Chantel <kobengchantel@gmail.com>
2025-12-13 14:52:34 +0200

    done

--- OCPJava/java8/Java8Practice/src/main/java/CarSelector.java
+++ OCPJava/java8/Java8Practice/src/main/java/CarSelector.java
@@ -1,2 +1,12 @@
 public class CarSelector {
+    public static void main(String[] arguments) {
+
+CarService carService = new CarService();
+        for(String argument : arguments) {
+             carService.process(argument);
+
+        }
+        return 42;
+
+    }
 }
---
+++ OCPJava/java8/Java8Practice/src/test/java/CarSelectorTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import org.junit.Test;
+
+public class CarSelectorTest {
+
+    @Test
+    public void shouldCallMain(){
+        String [] arguments = {"BMw", "Porsche"};
+        int returnValue = CarSelector.main(arguments);
+    }
+}
