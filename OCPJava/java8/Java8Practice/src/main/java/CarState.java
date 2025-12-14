a41c7c0bf870c45e02fd0c83208afbf4e1c14a9b
Kobeng Chantel <kobengchantel@gmail.com>
2025-12-14 19:37:28 +0200

    done

---
+++ OCPJava/java8/Java8Practice/src/main/java/CarState.java
@@ -0,0 +1,22 @@
+
+import java.io.IOException;
+
+public class CarState {
+    DRIVING, WAITING, PARKING;
+    public static void main from(String state){
+        switch(state) {
+            case "DRIVING"
+                ;
+                return DRIVING;
+            case "WAITING"
+                ;
+                return WAITING;
+            case "PARKING"
+                ;
+                return PARKING;
+            default:
+                throw new Exception();
+
+         }
+    }
+}
